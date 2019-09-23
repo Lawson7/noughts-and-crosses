bab1f331ed713e1a4f3ab36ca1630ad2f0176324
Harry Lawson <harryglawson@gmail.com>
2019-09-23 23:08:17 +1200

    two player functionality working

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
 //gameboard module
 const gameBoard = (() => {
 
-    let boardScore = ['X','0','X','X','X','0','0','0','X']
-    
+    let boardScore = ['','','','','','','','','']
 
-    return {boardScore}
+    const turnBox = document.querySelectorAll('.turnBox');
+
+    const updateBoard = function () {
+        turnBox.forEach((box) => {
+            box.textContent = boardScore[box.dataset.index];
+        });
+    };
+    
+    const boxSensor = function () {
+        turnBox.forEach((box) =>{
+            box.addEventListener('click', function() {
+                if (gameBoard.boardScore[box.dataset.index] == ''){
+                    if (game.playerOne.roundCount == game.playerTwo.roundCount) {
+                        game.playerOne.roundCount = game.playerOne.roundCount + 1;
+                        gameBoard.boardScore[box.dataset.index] = game.playerOne.symbol
+                    } else {
+                        game.playerTwo.roundCount = game.playerTwo.roundCount + 1;
+                        gameBoard.boardScore[box.dataset.index] = game.playerTwo.symbol
+                    }
+                } else {alert('spot taken')}
+                
+                gameBoard.updateBoard();
+            });
+        });
+    };
+
+
+    const detectWin = function (){
+
+    }
+
+    return {boardScore, updateBoard, boxSensor, detectWin}
 })();
 
-//player factory function
-const playerFactory = ((name) => {
 
-    return {name};
+
+//player factory function
+const playerFactory = ((name, symbol, roundCount) => {
+    return {name, symbol, roundCount};
 });
+
 
 //gameplay module
 const game = (() => {
+    gameBoard.updateBoard();
 
-const turnBox = document.querySelectorAll('.turnBox');
+    const playerOne = playerFactory('Human','X', 0);
+    const playerTwo = playerFactory('PC', 'O', 0);
 
-const updateBoard = function () {
-    turnBox.forEach((box) => {
-        box.textContent = gameBoard.boardScore[box.dataset.index];
-    });
-}
-    
-    return {updateBoard}
+    gameBoard.boxSensor();
+
+
+    gameBoard.detectWin();
+
+return {playerOne, playerTwo}
+
 })();
-
-game.updateBoard();
